cf6bd4075c969c4a573cae9e5a12f5f6497809be
Raisybear <elias.spycher@gmail.com>
2024-08-15 09:08:06 +0200

    Update

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+﻿namespace Vorporojekt_Gewinnverteilung;
+
+public class Menu
+{
+    public void Inputs()
+    {
+        Console.WriteLine("Willkommen bei unserem Gewinnverteilungsprogramm! Bitte geben Sie den Jahresgewinn ein: ");
+        int jahresGewinn = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Bitte geben Sie die Anzahl Aktien ein: ");
+        int aktien = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine("Bitte geben Sie das Partizipationskapital ein: ");
+        int partizipationskapital = Convert.ToInt32(Console.ReadLine());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            
+            Menu menu = new Menu();
+            menu.Inputs();
         }
     }
 }
